45411169d10310bbe91cc06c0ebb443ddd01062f
Stanislav Pukha <s.v.pukha@gmail.com>
2025-08-14 19:40:33 +0300

    new menu options add

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,29 +1,36 @@
-import { Card, CardBody, CardHeader, Autocomplete, AutocompleteItem } from '@heroui/react';
+import { Card, CardBody, CardHeader, DatePicker, Checkbox } from '@heroui/react';
 import MealsAutocomplete from './mealsAutocomplete';
 
 export default function MenuTab() {
     return (
-        <div className='grid w-full grid-cols-3 gap-4'>
-            <Card className='h-[500px] w-[300px]'>
-                <CardHeader>Понеділок</CardHeader>
+        <div className='grid w-full grid-cols-6 gap-4'>
+            <div className='col-span-6 text-center'>Оберіть дати для меню</div>
+            <DatePicker className='col-span-3' isRequired label='Дата початку' />
+            <DatePicker className='col-span-3' isRequired label='Дата закінчення' />
+            <Card className='h-[500px] w-[300px] col-span-2'>
+                <CardHeader className='grid grid-cols-2'>
+                    <div className='col-span-1'>Понеділок</div>
+                    <div className='col-span-1 justify-end'><Checkbox /></div>
+
+                </CardHeader>
                 <CardBody>
                     <MealsAutocomplete placeholder="Перші страви" className='mb-2' dbMeals='meals_soup' />
                     <MealsAutocomplete placeholder="Другі страви" className='mb-2' dbMeals='meals_garnish' />
                 </CardBody>
             </Card>
-            <Card>
+            <Card className='h-[500px] w-[300px] col-span-2'>
                 <CardHeader>Вівторок</CardHeader>
                 <CardBody>Card Body</CardBody>
             </Card>
-            <Card>
+            <Card className='h-[500px] w-[300px] col-span-2'>
                 <CardHeader>Середа</CardHeader>
                 <CardBody>Card Body</CardBody>
             </Card>
-            <Card>
+            <Card className='h-[500px] w-[300px] col-span-2'>
                 <CardHeader>Четвер</CardHeader>
                 <CardBody>Card Body</CardBody>
             </Card>
-            <Card>
+            <Card className='h-[500px] w-[300px] col-span-2'>
                 <CardHeader>П'ятниця</CardHeader>
                 <CardBody>Card Body</CardBody>
             </Card>
