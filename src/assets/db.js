50fb6900f182f0724c4e5857291f7fd5844c8581
Stanislav <s.v.pukha@gmail.com>
2025-08-29 11:45:12 +0300

    work days now initialised in db file

--- src/assets/db.js
+++ src/assets/db.js
@@ -1,7 +1,7 @@
 import Dexie from 'dexie';
 
 export const db = new Dexie('dinnerDB');
-db.version(1).stores({
+db.version(2).stores({
   meals_soup: 'id, &value, &label',
   meals_garnish: 'id, &value, &label',
   meals_meat: 'id, &value, &label',
@@ -9,6 +9,21 @@ db.version(1).stores({
   meals_bakery: 'id, &value, &label',
   meals_drink: 'id, &value, &label',
   menu: 'id, monday, tuesday, wednesday, thursday, friday, dateFirst, dateLast',
+  work_days: '++id, state'
+});
+
+db.work_days.count().then(count => {
+  if (count === 0) {
+    db.work_days.bulkAdd([
+      { id: 1, state: false }, 
+      { id: 2, state: false }, 
+      { id: 3, state: false }, 
+      { id: 4, state: false }, 
+      { id: 5, state: false }, 
+      { id: 6, state: false }, 
+      { id: 7, state: false }
+    ]);
+  }
 });
 
 export default db;
